bd7f852cf73a5ff8c3c269ba879ba3679ab1dd1d
hugoPonthieu <hugo.ponthieu@etu.umontpellier.fr>
2024-03-23 21:25:38 +0100

    fix ansible

--- instance/output.tf
+++ instance/output.tf
@@ -1,3 +1,8 @@
 output "ip_cadavera" {
   value = openstack_compute_instance_v2.test_terraform_instance.access_ip_v4
 }
+
+resource "local_file" "foo" {
+  content  = "[mesmachines]\n ovhInstance ansible_user=debian ansible_host=${openstack_compute_instance_v2.test_terraform_instance.access_ip_v4}"
+  filename = "../setup/inventory"
+}
