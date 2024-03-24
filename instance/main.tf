5d3e98ff2e172024040bcbbcf01d68b7b56ebb21
hugoPonthieu <hugo.ponthieu@etu.umontpellier.fr>
2024-03-24 12:27:48 +0100

    adding variables

--- instance/main.tf
+++ instance/main.tf
@@ -1,3 +1,8 @@
 module "upServer" {
   source = "./upServer"
+  name_sshkey = "Name you want to key to take in your ovh account"
+  file_sshkey = "Path to a local public key"
+  application_key = "Generate app key"
+  application_secret = "Generate app secret"
+  consumer_key = "Generate consumer key"
 }
--- instance/upServer/variables.tf
+++ instance/upServer/variables.tf
@@ -1,6 +1,15 @@
 variable "name_sshkey" {
-  type=string
+  type = string
 }
 variable "file_sshkey" {
-  type=string
+  type = string
+}
+variable "application_key" {
+  type = string
+}
+variable "application_secret" {
+  type = string
+}
+variable "consumer_key" {
+  type = string
 }
