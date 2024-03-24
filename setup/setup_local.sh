e568fc46393b130b2056a1fe46c7cbe290f20a7c
hugoPonthieu <hugo.ponthieu@etu.umontpellier.fr>
2024-03-24 11:29:46 +0100

    adding bash script for local setup

---
+++ setup/setup_local.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+mv ovhInstance/etc/rancher/k3s/k3s.yaml ./k3s.yaml
+url=$(cat k3s_url)
+rm k3s_url
+rm -r ovhInstance
+kubectl --kubeconfig k3s.yaml config set-cluster default --server="$url"
